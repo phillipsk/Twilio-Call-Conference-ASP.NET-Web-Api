ed6dca73d0f164897c2844ec0be51b571becd513
phillipsk <kjphillips@suffolk.edu>
2014-12-01 00:04:56 -0500

    configured for remote deploy
    edited say responses as well

--- Twilio3/ApiControllers/ConferenceController.cs
+++ Twilio3/ApiControllers/ConferenceController.cs
@@ -2,6 +2,7 @@
 
 using System.Net.Http;
 using System.Text;
+using System.Web.Configuration;
 using System.Web.Http;
 using Twilio.Mvc;
 using Twilio.TwiML;
@@ -14,20 +15,31 @@ namespace Twilio3.ApiControllers
     {
         [HttpPost]
         public HttpResponseMessage Call(TwilioRequest voiceRequest)
+        //  api/conference/call
         {
             var twilioResponse = new TwilioResponse();
 
-            const string actionUrl = "http://longle.ngrok.com/api/Conference/User";
+            var callUrl= WebConfigurationManager.AppSettings.Get("callUrl");
+            var userUrl= WebConfigurationManager.AppSettings.Get("userUrl");
 
-            twilioResponse.BeginGather(new {action = actionUrl, finishOnKey = "#", method = "POST"});
+            twilioResponse.BeginGather(new {action = userUrl, finishOnKey = "#", method = "POST"});
 
             twilioResponse.Say(@"
-                Press 1 to join as listener, 
-                Press 2 to join as speaker, 
-                Press 3 to join as moderator",
+                Welcome to the Prayer Line of Fellowship Mission Church. Please press 1 at any time to join the Prayer Line. 
+Our Congregation is centered between the historic Fort Hill neighborhood and directly behind Roxbury Community College.
+We are steps away from the Orange Line Roxbury Crossing T Station. Our direct address is 85 Centre Street, Roxbury, 02119.
+
+                  
+                Press 3 to join as a prayer coordinator",
                 new {voice = "woman", numOfDigits = 1});
+            //Press 2 to join as speaker,
 
             twilioResponse.EndGather();
+
+            twilioResponse.Say(@"
+                We did not receive a response", new { voice = "woman" });
+
+            twilioResponse.Redirect(callUrl);
 
             return CreateResponseMessage(twilioResponse);
         }
@@ -48,6 +60,9 @@ namespace Twilio3.ApiControllers
                 case "3":
                     twilioResponse.DialConference("myRoom", new { startConferenceOnEnter = true, endConferenceOnExit = true, muted = false });
                     break;
+                //case "3":
+                //   twilioResponse.DialConference("myRoom", new { startConferenceOnEnter = true, endConferenceOnExit = true, muted = false });
+                //    break;
             }
 
             return CreateResponseMessage(twilioResponse);
